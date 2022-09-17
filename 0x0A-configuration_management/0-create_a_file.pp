b509856f435e6e66904e4e450cac17350df76e1f
AnitaMatt <arneethar16@yahoo.com>
2022-09-17 10:36:51 -0700

    creating a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,11 @@
+# This isa puppet manifest that creates a file in /tmp
+
+
+file { 'school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content =>  'I love Puppet'
+}
